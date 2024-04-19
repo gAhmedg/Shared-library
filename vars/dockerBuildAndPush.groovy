de63c838b5562c16a2b3f3152df137524c6b8a3e
Ahmed Gomaa <gomaaa447@gmai.com>
2024-04-19 17:27:15 +0200

    edit vars

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,9 +1,6 @@
 #!usr/bin/env groovy
-def call(String dockerHubCredentialsID, String imageName) {
-	// Log in to DockerHub 
-	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        }
+def call( String imageName) {
+	
         
         // Build and push Docker image
         echo "Building and Pushing Docker image..."
