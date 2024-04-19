375651a9fd3f7a903c3fda87b313b636ec550052
Ahmed Gomaa <gomaaa447@gmai.com>
2024-04-19 18:19:57 +0200

    edit old

--- vars/oldDockerBuildAndPush.groovy
+++ vars/oldDockerBuildAndPush.groovy
@@ -1,12 +1,5 @@
-def call(Map config [:]) {
-  
-
+def call(Map config = [:]) {
   sh "docker build --tag ${config.imageName} ."
   echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-  sh "docker push ${config.imageName}"	
-  
- 
-
+  sh "docker push ${config.imageName}"
 }
-
- 
