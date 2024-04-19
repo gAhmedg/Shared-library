f2d9c0e969555fa5a1da9b3a237cc0edb3aa981f
Ahmed Gomaa <gomaaa447@gmai.com>
2024-04-19 17:56:56 +0200

    edit old

--- vars/oldDockerBuildAndPush.groovy
+++ vars/oldDockerBuildAndPush.groovy
@@ -1,23 +1,12 @@
 def call(Map config [:]) {
   
+
+  sh "docker build --tag ${config.imageName} ."
+  echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+  sh "docker push ${config.imageName}"	
   
-  sh """
-    #Build the image
-    docker build --tag ${config.imageName} .
-
-    #login before  pushing to registry if required
-    echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-
-    #push images to registery
-    docker push ${config.imageName}
-    docker logout
-
-  """
+ 
 
 }
 
-
  
-/*
-dockerBuildAndPush( imageName: "", port : "")
-*/
