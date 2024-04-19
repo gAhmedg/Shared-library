00327df816677381076f139ba7e0833626e697f7
Ahmed Gomaa <gomaaa447@gmai.com>
2024-04-19 18:42:59 +0200

    edit old

--- vars/oldDockerBuildAndPush.groovy
+++ vars/oldDockerBuildAndPush.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
-  sh "docker build --tag ${config.imageName} ."
+  sh "docker build --tag ${config.image} ."
   withCredentials([usernamePassword(credentialsId: config.DockerhubCredentials, passwordVariable: 'password', usernameVariable: 'user')]) {
     sh "docker login -u ${user} -p ${password}"
 }
-  sh "docker push ${config.imageName}"
+  sh "docker push ${config.image}"
 }
