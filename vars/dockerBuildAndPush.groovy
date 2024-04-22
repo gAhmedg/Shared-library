02cd587c948c604f776fe85a5c1011a4465243c2
Ahmed Gomaa <gomaaa447@gmai.com>
2024-04-22 18:18:54 +0200

    s

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,11 +1,12 @@
 def call(Map config = [:]) {
-  sh "docker build --tag ${config.image} ."
-
- 
-
-  withCredentials([usernamePassword(credentialsId: "config.DockerCredentials", passwordVariable: 'password', usernameVariable: 'user')]) {
-   sh "docker login -u ${user} -p ${password}"
-
-}
-  sh "docker push ${config.image}"
+    // Build the Docker image
+    sh "docker build --tag ${config.image} ."
+    
+    // Login to Dockerhub using provided credentials
+    withCredentials([usernamePassword(credentialsId: config.DockerCredentials, passwordVariable: 'password', usernameVariable: 'user')]) {
+        sh "docker login -u ${user} -p ${password}"
+    }
+    
+    // Push the built image to Dockerhub
+    sh "docker push ${config.image}"
 }
