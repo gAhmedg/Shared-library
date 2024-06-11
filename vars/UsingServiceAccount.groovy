861931a47ab792a9e873522045ed84473e481c3e
Ahmed-Gomaa <108756442+gAhmedg@users.noreply.github.com>
2024-06-11 17:50:55 +0300

    Update UsingServiceAccount.groovy

--- vars/UsingServiceAccount.groovy
+++ vars/UsingServiceAccount.groovy
@@ -1,1 +1,12 @@
+def call(Map config = [:]) {
+
+ withKubeCredentials(kubectlCredentials: [[caCertificate: '',
+                                           clusterName: '',
+                                           contextName: '',
+                                           credentialsId: config.credentialsId,
+                                           namespace: '',
+                                           serverUrl: config.ClusterUrl]])
+                {    
+                 sh "kubectl apply -f ${config.file} -n ${config.namespace}" 
+                        }
 
